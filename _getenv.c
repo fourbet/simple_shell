4d5a6dea7998b76ff42dca1073bf5aa5b7b37d0e
fourbet <ofourbet@yahoo.fr>
2020-11-17 10:11:32 +0000

    add _getenv and modify type_prompt

---
+++ _getenv.c
@@ -0,0 +1,41 @@
+#include "holberton.h"
+
+char *_getenv(const char *name)
+{
+	extern char **environ;
+        char *env = NULL;
+	char *res = NULL;
+	int i = 0;
+	int len = 0;
+	char **envPtr;
+
+	envPtr = malloc(sizeof(char *) * 32);
+	
+	while(environ[i])
+	{
+		envPtr[i] = strdup(environ[i]);
+		i++;
+	}
+	envPtr[i] = NULL;
+	i = 0;
+	while (envPtr[i] != NULL)
+	{
+		res = strtok(envPtr[i], "=\n");
+		if ((strcmp(res, name) == 0))
+		{
+			res = strtok(NULL, "=\n");
+			break;
+		}
+		i++;
+	}
+	env = strdup(res);
+	i = 0;
+	while (envPtr && envPtr[i])
+	{
+		free(envPtr[i]);
+		i++;
+	}
+	free(envPtr);
+        return (env);
+}
+
