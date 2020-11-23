63b7056fc6f791e37f25acc0176ef9b849a0bd19
fourbet <ofourbet@yahoo.fr>
2020-11-23 18:17:12 +0000

    delete unused variables

--- _getenv.c
+++ _getenv.c
@@ -4,7 +4,6 @@ char *_strdelete(char *string)
 {
         char *s;
         int i = 0;
-        int j = 0;
 	int index = 0;
         s = malloc(sizeof(char) * _strlen(string));
 	while(string[i] != '\0')
@@ -29,14 +28,8 @@ char *_strdelete(char *string)
 
 char *_getenv(char *name, list_t **ptrEnv)
 {
-        extern char **environ;
-        char *env = NULL;
-        char *res = NULL;
-        int i = 0;
-        int passed = 0;
-        char **envPtr;
-        char *string;
-        list_t *current;
+	char *env = NULL;
+	list_t *current;
 
         current = *ptrEnv;
 
@@ -45,7 +38,6 @@ char *_getenv(char *name, list_t **ptrEnv)
                 if ((strncmp(current->str, name, _strlen(name)) == 0))
                 {
                         env = _strdelete(current->str);
-                        passed = 1;
                         break;
                 }
                 current = current->next;
